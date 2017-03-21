fd371c5d51d10599a92df4f7c3d03f492484abcf
Charles1104 <charles.xavier.verleyen@gmail.com>
2017-03-20 17:00:06 -1000

    all tests passed except insert

--- linkedList.js
+++ linkedList.js
@@ -5,33 +5,108 @@
  */
 function linkedListGenerator(){
 
-  var listOfNodes = [];
-  var node = {};
+  function node(value) {
+    var next = null;
+    return {
+      value,
+      next
+    };
+  }
+
+  var head = null;
+  var tail = null;
 
 
   function getHead(){
-    return listOfNode[0];
+    return head;
   }
 
   function getTail(){
-    return listOfNode[listOfNode.length - 1];
+    return tail;
   }
 
-  function add(Value){
-    listOfNodes.push(Value);
-    listOfNode[listOfNode.length - 2].next = Value;
-    listOfNode[listOfNode.length - 1].next = Null;
+  function add(value){
+    if (head === null){
+      tail = node(value);
+      head = tail;
+    }
+
+    else {
+      tail.next = node(value);
+      tail = tail.next;
+    }
+
+    return tail;
   }
 
-  function get(Number){
+  function get(number){
+
+  var current = 0;
+  var currentNode = head;
+
+    if (head === null){
+      return false;
+    }
+
+    while (current !== number){
+
+      if(currentNode.next === null){
+        return false;
+      }
+      else{
+        currentNode = currentNode.next;
+      }
+
+    current ++;
+    }
+
+  return currentNode;
 
   }
 
-  function remove(Number){
+  function remove(number){
+
+  var currentNode = get(number);
+  var previousNode = get(number - 1);
+
+  if (currentNode === false) {
+    return false;
+  }
+
+  if (number === 0) {
+    head = currentNode.next;
+  }
+
+  if (currentNode.next === null) {
+    tail = previousNode;
+    previousNode.next = currentNode.next;
+  }
+
+  else {
+  previousNode.next = currentNode.next;
+  }
 
   }
 
-  function insert(Value, Number){
+  function insert(value, number){
+
+    var currentNode = get(number - 1);
+    var nextNode = get(number + 1);
+    var nextHead = head.next;
+
+
+    if (number === 0) {
+    head = node(value);
+    head.next = nextHead;
+    console.log("head");
+    console.log(head.next);
+    }
+
+
+    else {
+      currentNode.next = node(value);
+      currentNode.next.next = nextNode;
+    }
 
   }
 
@@ -44,7 +119,13 @@ function linkedListGenerator(){
     insert
   };
 
+
 }
 
-var movies = linkedListGenerator();
+var animals = linkedListGenerator();
+animals.add("cat");
+animals.add("dog");
+animals.add("lion");
+
+console.log(animals.insert("elephant",0));
 
